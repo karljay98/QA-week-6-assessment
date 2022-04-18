42e34d2a9693f3327030ca4e0d55710f13bcdb97
karljay98 <karljace@msn.com>
2022-04-18 15:48:16 +0000

    forms added

---
+++ application/forms.py
@@ -0,0 +1,15 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, Length
+
+class OwnersForm(FlaskForm):
+    first_name = StringField('First name', validators=[DataRequired(), Length(max=50)])
+    last_name = StringField('Last name', validators=[Length(max=70)])
+    submit = SubmitField('Add Owner')
+
+
+class ShoesForm(FlaskForm):
+    shoe_name = StringField('Shoe name', validators=[DataRequired(), Length(max=40)])
+    shoe_size = IntegerField('Shoe size',validators=[DataRequired()])
+    shoe_colour = StringField('Shoe colour', validators=[Length(max=20)])
+    submit = SubmitField("Add shoe") 
