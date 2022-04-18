dbf40d8cbf7d701ea4b971baad6e31152dd38659
karljay98 <karljace@msn.com>
2022-04-18 18:20:22 +0000

    partial functionality

--- application/routes.py
+++ application/routes.py
@@ -1,10 +1,19 @@
 from application import app, db
 from application.forms import OwnersForm, ShoesForm
-from application.models import Owners, Shoes
-from flask import render_template, request, redirect, url_for
+#from application.models import Owners, Shoes
+from flask import render_template #, request, redirect, url_for
 
 
 @app.route('/index')
 def index():
-    all_owners = Owners.query.all()
-    return render_template('index.html', all_owners=all_owners)
+    return render_template('index.html')
+
+@app.route('/add_owner', methods=['GET', 'POST'])
+def add_owner():
+     form=OwnersForm()
+     return render_template('add_owner.html', form=form)
+
+@app.route('/add_shoe', methods=['GET', 'POST'])
+def add_shoe():
+    form=ShoesForm()
+    return render_template('add_shoe', form=form)
