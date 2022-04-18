b668e989e40c72a69c907f5db966b058f7f825e2
karljay98 <karljace@msn.com>
2022-04-18 15:29:37 +0000

    added models

---
+++ application/models.py
@@ -0,0 +1,14 @@
+from application import db
+
+class Owners(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(70), nullable=False)
+    shoes = db.relationship('Shoes', backref='owner')
+
+class Shoes(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    shoe_name = db.Column(db.String(40), nullable=False)
+    shoe_size = db.Column(db.Integer, nullable=False)
+    shoe_colour = db.Column(db.String(20), nullable=False)
+    owner_id = db.Column(db.Integer, db.ForeignKey('owner.id'), nullable=False)
